d632fa0e4c5527dbbf2443ab1608b568ce6fa469
minjeparkk <zone8572@gmail.com>
2025-08-31 15:12:51 +0900

    Fix
    감염도 UI 스타일 변경

--- Assets/02. Scripts/UI/GameUI.cs
+++ Assets/02. Scripts/UI/GameUI.cs
@@ -2,47 +2,37 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameUI : BaseUI
 {
-    private Slider infectionSlider;
+    public TextMeshProUGUI infectionNumber;
+
     private float maxInfection = 100f;
     private float currentInfection = 0f;
 
     public override void Init(UIManager uiManager)
     {
         base.Init(uiManager);
-
-        // 자식 오브젝트에서 감염도 Slider 컴포넌트 찾기
-        infectionSlider = GetComponentInChildren<Slider>();
     }
 
-    // 감염도 증가 함수 (인간/사냥꾼 죽을 때 호출)
-    public void IncreasInfection(float amount)
+    // 감염도 증가 (인간이 죽을때 호출)
+    public void getInfection(float amount)
     {
+        // 감염도를 amount만큼 증가시키고, maxInfection(100)을 넘지 않도록 제한
         currentInfection += amount;
-        
-        // 감염도가 최대치를 넘지 않도록 제한
         currentInfection = Mathf.Min(currentInfection, maxInfection);
 
-        Debug.Log($"Infection Increased : {currentInfection}");
+        // 감염도 UI 업데이트 함수 호출
+        SetInfectionNumber(currentInfection);
     }
 
-    // 현재 감염도 % 비교
-    public float GetPercentage()
+    // 감염도UI 업데이트 함수
+    public void SetInfectionNumber(float number)
     {
-        return currentInfection / maxInfection;
-    }
-
-    private void Update()
-    {
-        // 실시간 감염도 업데이트
-        if (infectionSlider != null)
-        {
-            infectionSlider.value = GetPercentage();
-        }
+        infectionNumber.text = number.ToString();
     }
 
     protected override UIState GetUIState()
