00fb32479250cb4c61d2b19da8b40cead28e693e
SJL2002 <sungjunlim13@gmail.com>
2025-09-01 03:18:39 +0900

    [ Feat ] Player + StateMachine
    설명: 플레이어 그리고 StateMachine 작성 시작

---
+++ Assets/02. Scripts/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerInput playerInputs { get; private set; }
+    public PlayerInput.PlayerMovementActions playerActions { get; private set; }
+    void Start()
+    {
+        playerInputs = new PlayerInput();
+        playerActions = playerInputs.PlayerMovement;
+    }
+
+    private void OnEnable()
+    {
+        playerInputs.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInputs.Disable();
+    }
+}
